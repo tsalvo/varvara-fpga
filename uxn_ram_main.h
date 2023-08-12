dea14c3eeaac0a8233d8203f0b5f14fd172efa60
Tom Salvo <thsalvo@icloud.com>
2023-08-12 14:08:10 -0700

    update main_ram to be dual port r/w

--- uxn_ram_main.h
+++ uxn_ram_main.h
@@ -3,67 +3,64 @@
 #pragma once
 #include "ram.h"      // PipelineC RAM declarations
 
-// Declare Main RAM (64KB)
-// dual port, first port is read+write,
-// second port is read only
-// 1 cycle(s) of latency
-DECL_RAM_DP_RW_R_1(
+
+// Dual port, two read+write ports, 1 clock latency
+// Does not have read enable separate from clock enable
+DECL_RAM_DP_RW_RW_1(
   uint8_t,  // Element type
   main_ram, // RAM function name
   65536,    // Number of elements
   RAM_INIT_INT_ZEROS // Initial value VHDL string, from ram.h
 )
 
-uint8_t main_ram_read(uint16_t address) {
-	static uint32_t main_r_rdaddr;       // Read address
-	main_r_rdaddr = (uint32_t)(address); // read 
+uint16_t main_ram_update(
+	uint16_t address0, 
+	uint8_t write_value0,
+	uint1_t write_enable0,
+	uint16_t address1,
+	uint8_t write_value1,
+	uint1_t write_enable1
+) {
+	static uint32_t rwaddr0; // Write+read address 1
+	static uint32_t rwaddr1; // Write+read address 2
+	rwaddr0 = (uint32_t)(address0);
+	rwaddr1 = (uint32_t)(address1);
 	
 	// The RAM instance
-	main_ram_outputs_t main_r_ram_out = main_ram(
-		0,              // read-write_addr
-		0,              // write data
-		0,              // write enable (RW port always writing (not reading))
-		0,              // valid read-write input
-		0,              // read-write (read) enable 0
-		main_r_rdaddr,  // read address
-		1,              // valid read input
-		1				// read enable 1
+	uint1_t valid0 = 1;
+	uint1_t valid1 = 1;
+	main_ram_outputs_t ram_out = main_ram(
+		rwaddr0, 
+		write_value0, 
+		write_enable0, 
+		valid0, 
+		rwaddr1, 
+		write_value1, 
+		write_enable1, 
+		valid1
 	);
-		
-	return main_r_ram_out.rd_data1;
-}
-
-void main_ram_write(uint16_t address, uint8_t value) {
-	static uint32_t main_w_rwaddr = 0; // Write+read address
-	static uint8_t  main_w_wdata = 0;  // Write data
-	main_w_rwaddr = (uint32_t)(address);
-	main_w_wdata = value;
 	
-	// The RAM instance
-	main_ram_outputs_t main_w_ram_out = main_ram(
-		main_w_rwaddr,   // read-write_addr
-		main_w_wdata,    // write data
-		1,               // write enable (RW port always writing (not reading))
-		1,               // valid read-write input
-		0,               // read-write (read) enable 0
-		0,               // read address
-		1,               // valid read input
-		0				 // read enable 1
-	);
+	return (uint16_t)((ram_out.rd_data0 << 8) | ram_out.rd_data1);
 }
 
-uint16_t peek2_ram(uint16_t address) {
-	static int32_t mem0; 
-	static uint16_t mem1; 
-	static uint16_t result; 
-	// cast to int32 to silence warning about integer promotion during bit shift
-	mem0 = (int32_t)(main_ram_read(address));
-	mem1 = (uint16_t)(main_ram_read(address + 1));
-	result = (uint16_t)(mem0 << 8) | mem1;
-	return result;
+void poke_ram(uint16_t address, uint8_t value) {
+	main_ram_update(address, value, 1, 0, 0, 0);
 }
 
 void poke2_ram(uint16_t address, uint16_t value) {
-	main_ram_write(address, (uint8_t)(value >> 8));
-	main_ram_write(address + 1, (uint8_t)(value));
+	static uint8_t value0;
+	static uint8_t value1;
+	value0 = (uint8_t)(value >> 8);
+	value1 = (uint8_t)(value);
+	main_ram_update(address, value, 1, address + 1, value1, 1);
+}
+
+uint8_t peek_ram(uint16_t address) {
+	static uint16_t ram_read;
+	ram_read = main_ram_update(0, 0, 0, address, 0, 0);
+	return (uint8_t)(ram_read);
+}
+
+uint16_t peek2_ram(uint16_t address) {
+	return main_ram_update(address, 0, 0, address + 1, 0, 0);
 }
